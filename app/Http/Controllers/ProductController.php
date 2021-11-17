3f82dfab4df1c29b4818abb0ddf5baaa896d97b9
Lorenzo <lorenzocardoso@hotmail.com>
2021-11-17 09:24:55 -0300

    implementando controller e validações básicas de produtos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::all();
+        return view('product.index', ['products' => $products]);
+    }
+
+    public function show($id)
+    {
+        $product = Product::query()->find('id', $id);
+        return view('product.show', ['product' => $product]);
+    }
+
+    public function create()
+    {
+        return view('product.create_edit');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate(Product::rules(), Product::feedback());
+
+        $product = $request->only('name', 'price', 'description');
+        Product::query()->create($product);
+
+        return redirect()->back()->with('message', 'Produto cadastrado com sucesso!');
+    }
+
+    public function edit($id)
+    {
+        $product = Product::query()->find('id', $id);
+        return view('product.create_edit', ['product' => $product]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate(Product::rules(), Product::feedback());
+
+        $product = Product::query()->find('id', $id);
+        $product->save($request->only('name', 'price', 'description'));
+
+        return redirect()->back()->with('message', 'Produto editado com sucesso!');
+    }
+
+    public function destroy($id)
+    {
+        Product::destroy($id);
+        return redirect()->route('product.index')->with('message', 'Produto deletado com sucesso!');
+    }
+
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,19 @@ class Product extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'price', 'description'];
+
+    public static function rules(){
+        return [
+          'name' => 'required',
+          'price' => 'required',
+          'description' => 'required|max:100'
+        ];
+    }
+
+    public static function feedback(){
+        return [
+          'required' => 'Preencha o campo!',
+          'max' => 'A descrição deve conter menos de 100 caracteres!'
+        ];
+    }
 }
