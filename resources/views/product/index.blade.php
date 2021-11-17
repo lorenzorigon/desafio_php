6b4439f162035421c08c0db20ccc662aa6108cad
Lorenzo <lorenzocardoso@hotmail.com>
2021-11-17 11:01:19 -0300

    implementando a view index de produtos

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,5 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-    INDEX
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header text-center">Produtos</div>
+                    <div class="card-body">
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Produto</th>
+                                <th scope="col">Valor</th>
+                                <th scope="col"></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($products as $product)
+                                <tr>
+                                    <th scope="row">{{$product->id}}</th>
+                                    <td>{{$product->name}}</td>
+                                    <td>{{$product->price}}</td>
+                                    <td>
+                                        <a href="#" class="btn btn-primary">Visualizar</a>
+                                        <a href="#" class="btn btn-success">Editar</a>
+                                        <a href="#" class="btn btn-danger">Excluir</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
