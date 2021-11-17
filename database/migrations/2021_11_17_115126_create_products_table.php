0fc6e8e0a294e05d4fc7f6b5ba7652ca14a1054b
Lorenzo <lorenzocardoso@hotmail.com>
2021-11-17 09:00:33 -0300

    criando os campos de produto na model e migration

--- database/migrations/2021_11_17_115126_create_products_table.php
+++ database/migrations/2021_11_17_115126_create_products_table.php
@@ -16,6 +16,9 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->float('price');
+            $table->string('description');
         });
     }
 
